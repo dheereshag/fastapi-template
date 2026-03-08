7613676f99b645db5dd389ebac31b09df6a48366
dheereshagr <dheeresh@dcluttr.ai>
2026-03-08 15:30:40 +0530

    feat: add user table creation and migration script

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from alembic import context
 
 from config import settings
 from database import Base
+import models  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
